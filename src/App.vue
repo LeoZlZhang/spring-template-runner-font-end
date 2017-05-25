3edb0db4b650040b220a613f4020464d2312bbdb
sh00514 <ziliang.zhang@shenmajr.com>
2017-05-25 13:42:50 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-    <!--<div style="margin:40%">-->
-        <!--<v-popconfirm title="确定删除吗?" @confirm="confirm" @cancel="cancel">-->
-            <!--<a href="javascript:;">删除</a>-->
-        <!--</v-popconfirm>-->
-    <!--</div>-->
 </template>
 
 <script>
@@ -12,14 +7,6 @@
         data () {
             return {}
         },
-//        methods: {
-//            confirm: function () {
-//                this.$message.info('点击了是');
-//            },
-//            cancel: function () {
-//                this.$message.info('点击了否');
-//            }
-//        }
     }
 </script>
 
